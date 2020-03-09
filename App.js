1814e2ad130aab531cbe0e46cc29d36802ce18b5
david9487 <stu20431@gmail.com>
2020-03-09 23:04:42 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>鞏持靚/110519011</Text>
+      <Text>I love sushi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
+  },
+});
